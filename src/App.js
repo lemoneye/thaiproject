e5197d6bb8d8db1d05e4a7cdf5fb3713985c0bd4
katekawaii <katekawaii1990@gmail.com>
2020-09-10 14:03:15 -0700

    Add Kate's Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import joe from '../src/Components/pages/joe';
 import home from '../src/Components/pages/home';
 import Noon from '../src/Components/pages/Noon';
+import Kate from '../src/Components/pages/Kate';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route exact path="/joe" component={joe} />;
           <Route exact path="/noon" component={Noon} />;
           <Route exact path="/" component={home} />;
+          <Route exact path="/kate" component={Kate} />;
         </Switch>
       </Router>
 
